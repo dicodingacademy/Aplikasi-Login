2001572496c2e852d837e0d0c2443add05e86842
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-06-05 15:53:08 +0700

    remove home script

--- assets/scripts/home-script.js
+++
@@ -1,6 +0,0 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
