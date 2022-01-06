6417934eaa766c65fe7e8a3f40b353e1f738c5f4
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2022-01-06 14:37:00 +0700

    Update login-script.js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -4,3 +4,21 @@
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
+
+const loginFormElement = document.querySelector('#loginForm');
+const inputEmailElement = document.querySelector('#inputEmail');
+const inputPasswordElement = document.querySelector('#inputPassword');
+
+const expectedEmail = 'admin@dicoding.com';
+const expectedPassword = 'superpassword';
+
+loginFormElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+    // TODO 1 : Mendapatkan input email dan password pengguna dari form
+  
+    /*  TODO 2 : Buat Logika perbandingan dengan kondisi:
+        Jika variabel email identik dengan expectedEmail dan password   identik dengan expectedPassword,
+        panggil fungsi goToHome()
+        Jika tidak, maka panggil fungsi showPopUp()
+     */
+});
