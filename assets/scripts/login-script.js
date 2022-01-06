de31fdfc1099e6b9a3ae70c91b5c3f3f56936c27
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2022-01-06 14:37:43 +0700

    Update login-script.js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -14,11 +14,10 @@ const expectedPassword = 'superpassword';
 
 loginFormElement.addEventListener('submit', function(event) {
     event.preventDefault();
-    // TODO 1 : Mendapatkan input email dan password pengguna dari form
+    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
   
-    /*  TODO 2 : Buat Logika perbandingan dengan kondisi:
-        Jika variabel email identik dengan expectedEmail dan password   identik dengan expectedPassword,
-        panggil fungsi goToHome()
-        Jika tidak, maka panggil fungsi showPopUp()
+    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
+       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
+       Jika tidak, maka panggil fungsi showPopUp().
      */
 });
