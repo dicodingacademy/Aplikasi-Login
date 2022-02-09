b4232dde9ad1871a6000d86007dbf1cb89012a23
iyaindah <75407046+iyaindah@users.noreply.github.com>
2022-02-09 11:51:54 +0800

    Add files via upload

---
+++ assets/scripts/login-script.js
@@ -0,0 +1,29 @@
+/*
+ * Catatan:
+ * Gunakan fungsi goToHome() untuk menampilkan halaman home
+ * Gunakan fungsi goToLogin() untuk menampilkan halaman login
+ * Gunakan fungsi showPopUp() untuk menampilkan pop up error
+ */
+
+const loginFormElement = document.querySelector('#loginForm');
+const inputEmailElement = document.querySelector('#inputEmail');
+const inputPasswordElement = document.querySelector('#inputPassword');
+
+const expectedEmail = 'admin@dicoding.com';
+const expectedPassword = 'superpassword';
+
+loginFormElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
+    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
+       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
+       Jika tidak, maka panggil fungsi showPopUp().
+     */
+    if (email == expectedEmail && password == expectedPassword ) {
+      goToHome();
+    } else {
+      showPopUp();
+    }
+});
