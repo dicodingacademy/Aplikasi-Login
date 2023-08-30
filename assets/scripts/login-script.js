ed9d4b2283dfebb4186b61ce59c850fd4c3a1e75
Muhamad Ilhan <92379841+Nyanta-23@users.noreply.github.com>
2023-08-30 16:46:55 +0700

    Update login-script.js
    Update komentar JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,9 +1,3 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
 /** @module Login-Script */
 /** 
 * Membuat variabel loginFormElement untuk tampilan form.
