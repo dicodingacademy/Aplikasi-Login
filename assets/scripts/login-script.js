27a9e2db92d973c7a7e52a8954f7f4f7e2257db0
SefranSani <145819454+SefranSani@users.noreply.github.com>
2023-09-23 12:12:14 +0700

    Update login-script.js
    cekekekekekeke

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -4,36 +4,36 @@
 * @constant {HTMLElement}
 */
 const loginFormElement = document.querySelector('#loginForm');
- 
- 
+
+
 /** 
 * Membuat variabel inputEmailElement untuk tampilan input email.
 * @constant {HTMLElement}
 */
 const inputEmailElement = document.querySelector('#inputEmail');
- 
- 
+
+
 /** 
 * Membuat variabel inputPasswordElement untuk tampilan input password.
 * @constant {HTMLElement}
 */
 const inputPasswordElement = document.querySelector('#inputPassword');
- 
- 
+
+
 /** 
  * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
  * @constant {string}
  */
 const expectedEmail = 'admin@dicoding.com';
- 
- 
+
+
 /** 
  * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
  * @constant {string}
  */
 const expectedPassword = 'superpassword'
- 
- 
+
+
 /* Comment :  Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -49,18 +49,18 @@ loginFormElement.addEventListener('submit', function(event) {
    * @constant {string}
    */
   const password = inputPasswordElement.value;
- 
- 
+
+
   /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
- 
+
     /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
- 
+
   } else {
- 
+
     /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
     showPopUp();
- 
+
   }
 });
